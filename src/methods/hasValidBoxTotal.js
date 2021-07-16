d44e8dd69ba0a344cfcdf8a43089847c12131c16
RaptorCentauri <dave.leo.shilander@icloud.com>
2021-07-16 02:22:39 +0000

    hasValidBoxTotal()

---
+++ src/methods/hasValidBoxTotal.js
@@ -0,0 +1,28 @@
+const hasValidBoxTotal= function(box) {
+	const boxIndex = {
+		1: 0,
+		2: 3,
+		3: 6,
+		4: 27,
+		5: 30,
+		6: 33,
+		7: 54,
+		8: 57,
+		9: 60,
+	}
+
+	const flatSudoku = this._sudoku.flat()
+
+	const cell = boxIndex[box]
+
+	const sumCells = (a,b)=>a+b
+
+	let total =
+		flatSudoku.slice(cell+0, cell+0+3).reduce(sumCells) +
+		flatSudoku.slice(cell+9, cell+9+3).reduce(sumCells) +
+		flatSudoku.slice(cell+18, cell+18+3).reduce(sumCells)
+		
+	return total === 45
+}
+
+export default hasValidBoxTotal
