23bdd1f8c5a95acc671131952e06f811b7fbf525
RaptorCentauri <dave.leo.shilander@icloud.com>
2021-07-16 02:21:22 +0000

    helper file to manage method exports

---
+++ src/methods/index.js
@@ -0,0 +1,19 @@
+import hasValidRowTotal from './hasValidRowTotal.js'
+import hasValidTotal from './hasValidTotal.js'
+import hasValidColumnTotal from './hasValidColumnTotal.js'
+import hasValidBoxTotal from './hasValidBoxTotal.js'
+import getRow from './getRow.js'
+import getColumn from './getColumn.js'
+import getBox from './getBox.js'
+
+
+export {
+	hasValidTotal,
+	hasValidRowTotal,
+	hasValidColumnTotal,
+	hasValidBoxTotal,
+	getRow,
+	getColumn,
+	getBox
+}
+
