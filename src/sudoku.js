32135a7feb37a658adb70d5dbd34406f5f3aadff
RaptorCentauri <dave.leo.shilander@icloud.com>
2021-07-22 18:23:22 +0000

    remove proxy from sudoku

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,10 +1,8 @@
 import * as index from './methods/index.js'
-import handler from './handler/handler.js'
 
 class Sudoku {
 	constructor(sudoku) {
 		this._sudoku = sudoku
-		return new Proxy(this, handler)
 	}
 	
 	hasValidTotal = index.hasValidTotal
